9bbc09fd96b6560a2b7e65488a98f0abe57aa87f
ESloman <elliot.sloman@hotmail.com>
2021-02-13 07:45:15 +0000

    Move the "main" code into __name__ == "__main__" block. Added the "gift eddies" command Ensure slash commands aren't global

--- discordbot/main.py
+++ discordbot/main.py
@@ -1,9 +1,6 @@
-import os
-
 import discord
 import discord_slash
 import dotenv
-import inflect
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils import manage_commands
@@ -12,33 +9,31 @@ from discordbot.commandmanager import CommandManager
 from discordbot.embedmanager import EmbedManager
 from mongo.bsepoints import UserPoints, UserBets
 
-TOKEN = dotenv.get_key(".env", "DISCORD_TOKEN")
-BETA_MODE = dotenv.get_key(".env", "BETA_MODE")
-
-if BETA_MODE is None:
-    BETA_MODE = True
-else:
-    BETA_MODE = False
-
-if BETA_MODE is False:
-    IDS = [291508460519161856]  # test IDs
-else:
-    BETA_MODE = True
-    IDS = [181098823228063764]  # actual IDS
-
-intents = discord.Intents.all()
-
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-
-com = CommandManager(client, IDS, beta_mode=BETA_MODE)
-embeds = EmbedManager()
-slash = com.slash
-
-
-USERBETS = UserBets(IDS)
-
 
 if __name__ == "__main__":
+    TOKEN = dotenv.get_key(".env", "DISCORD_TOKEN")
+    BETA_MODE = dotenv.get_key(".env", "BETA_MODE")
+
+    if BETA_MODE is None:
+        BETA_MODE = True
+    else:
+        BETA_MODE = False
+
+    if BETA_MODE is False:
+        IDS = [291508460519161856]  # test IDs
+    else:
+        BETA_MODE = True
+        IDS = [181098823228063764]  # actual IDS
+
+    intents = discord.Intents.all()
+
+    client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    com = CommandManager(client, IDS, beta_mode=BETA_MODE)
+    embeds = EmbedManager()
+    slash = com.slash
+
+    user_bets = UserBets(IDS)
+
     client.run(TOKEN)

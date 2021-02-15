00c2dded6645a9c55a7953b50eba94a28876d4fd
ESloman <elliot.sloman@hotmail.com>
2021-02-15 14:41:05 +0000

    Commit beginnings of passive eddie distribution

--- discordbot/betmanager.py
+++
@@ -1,7 +0,0 @@
-from mongo.bsepoints import UserPoints, UserBets
-
-
-class BetManager(object):
-    def __init__(self):
-        self.user_bets = UserBets()
-        self.user_points = UserPoints()

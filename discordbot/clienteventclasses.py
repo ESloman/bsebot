de344a9f13d558ae680530037325ce9740f71f72
ESloman <elliot.sloman@hotmail.com>
2021-04-11 07:45:20 +0100

    Add support for members leaving the server

--- discordbot/clienteventclasses.py
+++ discordbot/clienteventclasses.py
@@ -114,6 +114,34 @@ class OnMemberJoin(BaseEvent):
         self.logger.info(f"Creating BSEddies account for new user - {user_id} - {member.display_name}")
 
 
+class OnMemberLeave(BaseEvent):
+    """
+    Class for handling when a member leaves the server
+    """
+    def __init__(self, client, guild_ids, logger, beta_mode=False):
+        super().__init__(client, guild_ids, logger, beta_mode=beta_mode)
+
+    def on_leave(self, member: discord.Member) -> None:
+        """
+        Method for handling when a member leaves the server.
+        We basically just make sure that the user entry is set to inactive
+        :param member:
+        :return: None
+        """
+        user_id = member.id
+        self.user_points.update({"uid": user_id, "guild_id": member.guild.id}, {"$set": {"inactive": True}})
+        self.user_points.append_to_activity_history(
+            user_id,
+            member.guild.id,
+            {
+                "type": ActivityTypes.SERVER_LEAVE,
+                "timestamp": datetime.datetime.now()
+            }
+        )
+        self.logger.info(f"Deactivating BSEddies account for user - {user_id} - {member.display_name}")
+        return
+
+
 class OnReactionAdd(BaseEvent):
     """
     Class for handling on_reaction_add events from Discord

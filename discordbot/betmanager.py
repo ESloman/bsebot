6017fa565893fcf08eb75db004cea5ea2850d882
ESloman <elliot.sloman@hotmail.com>
2022-10-03 08:41:06 +0100

    Tax bet winnings
    Some changes to tax bet winnings the tax rate too
    Only taxing the actual winnings though - so user always gets what they bet back plus the taxed bet amount
    Bet winnings modifiers are now randomised a bit to give a bit more spread on the winnings (pretending there's variable odds)

--- discordbot/betmanager.py
+++ discordbot/betmanager.py
@@ -1,9 +1,11 @@
 import datetime
 import math
+import random
 
 from discordbot.bot_enums import TransactionTypes
 from discordbot.constants import BET_OUTCOME_COUNT_MODIFIER
 from mongo.bsepoints import UserBets, UserPoints
+from mongo.bsedataclasses import TaxRate
 
 
 class BetManager(object):
@@ -11,6 +13,7 @@ class BetManager(object):
         self.logger = logger
         self.user_bets = UserBets()
         self.user_points = UserPoints()
+        self.tax_rate = TaxRate()
 
     def close_a_bet(self, bet_id: str, guild_id: int, emoji: str) -> dict:
         """
@@ -52,7 +55,7 @@ class BetManager(object):
         elif modifier > 2:
             modifier = 2
 
-        point_one = (0, 2)
+        point_one = (0, 2 + (random.random() / 2))
         point_two = (winning_outcome_eddies, modifier)
         try:
             m = ((point_two[1] - point_one[1]) / (point_two[0] - point_one[0]))
@@ -60,6 +63,8 @@ class BetManager(object):
             m = 0
             # this mean no-one won :(
         c = (point_two[1] - (m * point_two[0]))
+        
+        self.logger.info(f"Bet {bet_id} winnings has modifier {m} and coefficient {c}")
 
         # get eddies the losers bet
         try:
@@ -67,6 +72,14 @@ class BetManager(object):
         except Exception as e:
             _extra_eddies = 0
 
+        # get tax value
+        tax_value = self.tax_rate.get_tax_rate()
+        
+        # total eddies won and total taxes
+        total_eddies_winnings = 0
+        total_eddies_won = 0
+        total_eddies_taxed = 0
+        
         # assign winning points to the users who got the answer right
         winners = [b for b in ret["betters"] if ret["betters"][b]["emoji"] == emoji]
         for better in winners:
@@ -81,6 +94,12 @@ class BetManager(object):
                 points_won += int(math.floor(_extra_eddies / len(winners)))
             except Exception:
                 pass
+           
+            total_eddies_won += points_won
+            actual_amount_won = points_won - points_bet  # the actual winnings without original bet
+            total_eddies_winnings += actual_amount_won
+            tax_amount = actual_amount_won - math.floor((actual_amount_won * tax_value))
+            total_eddies_taxed += tax_amount
 
             ret_dict["winners"][better] = points_won
             self.user_points.increment_points(int(better), guild_id, points_won)
@@ -96,4 +115,29 @@ class BetManager(object):
                 }
             )
 
+        # give taxed eddies to the King
+        self.logger.info(
+            f"Bet ID: {bet_id}\n"
+            f"Eddies won: {total_eddies_winnings}\n"
+            f"Eddies won (with original bets): {total_eddies_won}\n"
+            f"Eddies taxed: {total_eddies_taxed}"
+        )
+        
+        king_id = self.user_points.get_current_king(guild_id)["uid"]
+        self.user_points.increment_points(king_id, guild_id, total_eddies_taxed)
+        self.user_points.append_to_transaction_history(
+            king_id,
+            guild_id,
+            {
+                "type": TransactionTypes.BET_WIN,
+                "amount": total_eddies_taxed,
+                "timestamp": datetime.datetime.now(),
+                "bet_id": bet_id,
+            }
+        )
+        
+        ret_dict["king_tax"] = total_eddies_taxed
+        ret_dict["king"] = king_id
+        ret_dict["total_winnings"] = total_eddies_winnings
+        
         return ret_dict

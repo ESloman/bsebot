f9e2403f6b91ca175a512d171bf5ce1d7cac0160
esloman <elliot.sloman@hotmail.com>
2023-01-11 16:56:39 +0000

    Switch to using css selectors (#207) (#209)
    This removes the reliance on specific class names for certain elements and hopefully means that we're a bit more resilient for when Wordle gets updated and the randomly generated class names change.
    Additionally (#201), remove some files that aren't used anymore and (#208), add the placeholder logger object we can use throughout the codebase.

--- discordbot/wordle_stuff.py
+++
@@ -1,9 +0,0 @@
-from discordbot.wordle.wordlesolver import WordleSolver
-
-if __name__ == "__main__":
-    answers = []
-    for i in range(25):
-        solver = WordleSolver()
-        answer = solver.solve()
-        answers.append(answer)
-    print(answers)

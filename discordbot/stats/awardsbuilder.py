7c20bcefadd0a6eb5a16a3a31200d3bd88296425
ESloman <elliot.sloman@hotmail.com>
2023-02-01 19:52:46 +0000

    #261 - Fix BSEddies awards bug

--- discordbot/stats/awardsbuilder.py
+++ discordbot/stats/awardsbuilder.py
@@ -115,10 +115,10 @@ class AwardsBuilder:
         stat_parts = [
             message_start,
             (f"**Number of messages sent** 📬: `{number_messages.value}` "
-             f"(in `{number_messages.channels}` channel{'s' if len(thread_messages.channels) != 1 else ''} "
+             f"(in `{number_messages.channels}` channel{'s' if thread_messages.channels != 1 else ''} "
              f"from `{number_messages.users}` users)\n"),
             (f"**Number of thread messages sent** 📟: `{thread_messages.value}` "
-             f"(in `{thread_messages.channels}` thread{'s' if len(thread_messages.channels) != 1 else ''} "
+             f"(in `{thread_messages.channels}` thread{'s' if thread_messages.channels != 1 else ''} "
              f"from `{thread_messages.users}` users)\n"),
             (f"**Average message length** 📰: Characters (`{avg_message_chars.value}`), "
              f"Words (`{avg_message_words.value}`)\n"),

4df6945dd15cbcb679f46ef85eb8a59f3cd02074
brods <bdsimpleton@gmail.com>
2023-02-24 16:48:41 +0000

    Update dailyvallytask.py (#309)
    * Update dailyvallytask.py
    
    * Update dailyvallytask.py

--- discordbot/tasks/dailyvallytask.py
+++ discordbot/tasks/dailyvallytask.py
@@ -21,10 +21,15 @@ class AfterWorkVally(commands.Cog):
             "Anyone playing after-work {role} today?",
             "Who's about for after-work {role}?",
             "Anyone wanna get salty playing {role}?",
-            "Who's gonna grind some `Breeze` today {role}?",
+            "Who's gonna grind some `Lotus` today {role}?",
             "Anyone want to lose some RR {role}?",
             "Who wants to roll some fat 1s playing {role}?",
-            "Can we get an after-work 5-stack today for {role}?"
+            "Can we get an after-work 5-stack today for {role}?",
+            "My pp is soft, but my Valorant is hard. Someone play a game with me, {role}?",
+            "Jingle bells, jingle bells, jingle all the IT'S TIME TO PLAY VALORANT {role}",
+            "Valorant? Valorant? VALORANT? VALROARANT? RAVALROANT? {role}",
+            "I'm a little bot, it's time to get mad and play {role}?",
+            "Balls. {role}"
         ]
 
     def _check_start_up_tasks(self) -> bool:

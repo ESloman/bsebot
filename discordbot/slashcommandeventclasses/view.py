5547f8cb0776521c3a2ecb1209f6648b92f0da9a
Elliot Sloman <elliot.sloman@hotmail.com>
2023-03-28 11:26:28 +0100

    #322 - Application commands and direct messages
    - Stop _all_ slash commands from working in DMs unless the `self.dmable` attribute is set.
    - Refactor `/view` to let it work in DMs; to prove that we can have functional slash commands in direct messages.
    - Add extra functionality to guild sync that keeps the name updated
    - Updated GuildDB type to add name attribute

--- discordbot/slashcommandeventclasses/view.py
+++ discordbot/slashcommandeventclasses/view.py
@@ -3,6 +3,7 @@ import discord
 
 from discordbot.bot_enums import ActivityTypes
 from discordbot.slashcommandeventclasses.bseddies import BSEddies
+from mongo.datatypes import User
 
 
 class BSEddiesView(BSEddies):
@@ -12,6 +13,29 @@ class BSEddiesView(BSEddies):
 
     def __init__(self, client, guilds, logger):
         super().__init__(client, guilds, logger)
+        self.dmable = True
+
+    def _construct_guild_message(self, user: User) -> str:
+        """
+        Constructs the `View` message for a given user and guild
+
+        Args:
+            user (User): the DB Uer object
+
+        Returns:
+            str: the constructed message
+        """
+        guild_db = self.guilds.get_guild(user["guild_id"])
+        if not guild_db:
+            return "Guild didn't exist in guilds collection - probbaly just a debug error"
+        pending = self.user_bets.get_user_pending_points(user["uid"], user["guild_id"])
+        msg = (
+            f"**{guild_db['name']}**\n"
+            f"You have **{user['points']}** :money_with_wings:`BSEDDIES`:money_with_wings:!"
+            f"\nAdditionally, you have `{pending}` points on 'pending bets'.\n\n"
+            f"The _absolute highest_ amount of eddies you've ever had was `{user.get('high_score', 0)}`!."
+        )
+        return msg
 
     async def view(self, ctx: discord.ApplicationContext) -> None:
         """
@@ -26,13 +50,37 @@ class BSEddiesView(BSEddies):
         if not await self._handle_validation(ctx):
             return
 
+        _user = None
+        if not ctx.guild:
+            # no guild - workout if user is in more than one guild or not
+            users = self.user_points.find_user_guildless(ctx.author.id)
+            if len(users) > 1:
+                return await self.view_guildless(ctx, users)
+            elif len(users) == 1:
+                _user = users[0]
+            else:
+                # no guilds at all?
+                await ctx.respond("We do not share any guilds together.")
+                return
+
         self._add_event_type_to_activity_history(ctx.author, ctx.guild_id, ActivityTypes.BSEDDIES_VIEW)
 
-        ret = self.user_points.find_user(
-            ctx.author.id, ctx.guild.id, projection={"points": True, "high_score": True})
+        if not _user:
+            _user = self.user_points.find_user(ctx.author.id, ctx.guild.id)
 
-        pending = self.user_bets.get_user_pending_points(ctx.author.id, ctx.guild.id)
-        msg = (f"You have **{ret['points']}** :money_with_wings:`BSEDDIES`:money_with_wings:!"
-               f"\nAdditionally, you have `{pending}` points on 'pending bets'.\n\n"
-               f"The _absolute highest_ amount of eddies you've ever had was `{ret.get('high_score', 0)}`!.")
+        msg = self._construct_guild_message(_user)
         await ctx.respond(content=msg, ephemeral=True)
+
+    async def view_guildless(self, ctx: discord.ApplicationContext, users: list[User]) -> None:
+        """
+        Sends multiple messages for each guild the user is in
+
+        Args:
+            ctx (discord.ApplicationContext): the context to respond to
+            users (list[User]): the list of users
+        """
+        await ctx.respond("Eddies for all your guilds:")
+
+        for _user in users:
+            msg = self._construct_guild_message(_user)
+            await ctx.followup.send(content=msg)

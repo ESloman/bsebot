5f195ab6c6cad6ea957b7390ecd3253ed4e4926b
esloman <elliot.sloman@hotmail.com>
2023-04-18 16:48:08 +0100

    Fix typo

--- discordbot/tasks/wordlereminder.py
+++ discordbot/tasks/wordlereminder.py
@@ -28,7 +28,7 @@ class WordleReminder(BaseTask):
             "Don't forget to do your wordle today {mention}.",
             "{mention} hasn't done their Wordle today. Are they stupid?",
             "Daddy wants you to complete your Wordle today {mention}",
-            "{mention}\nRoses are red\nViolets are blue\nI've done my Worlde\nDon't forget to do yours too!",
+            "{mention}\nRoses are red\nViolets are blue\nI've done my Wordle\nDon't forget to do yours too!",
             "Hey {mention}, you absolute knob, you haven't done your Wordle yet!",
             "Guess what? {mention} is a fucking prick. Also, they didn't do their Wordle.",
             "Do your Wordle or die, {mention}."

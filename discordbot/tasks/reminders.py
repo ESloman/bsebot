1b2d40c8c1a50d02afca989eebef54671174dd73
esloman <elliot.sloman@hotmail.com>
2023-04-19 18:44:52 +0100

    #293 - Add /remindme functionality (#388)
    Add functionality for reminders. There are two ways to create a reminder:
    - using a slash command `/remindme`
    - right-clicking a message and using a context menu command
    Both methods will trigger a modal to be filled out. This will then enter a reminder into the database.
    
    A new task has been added to check for expired reminders.

---
+++ discordbot/tasks/reminders.py
@@ -0,0 +1,63 @@
+
+import asyncio
+import datetime
+from logging import Logger
+
+from discord.ext import tasks
+
+from discordbot.bsebot import BSEBot
+from discordbot.tasks.basetask import BaseTask
+
+
+class RemindersTask(BaseTask):
+    def __init__(
+        self,
+        bot: BSEBot,
+        guild_ids: list[int],
+        logger: Logger,
+        startup_tasks: list[BaseTask]
+    ):
+
+        super().__init__(bot, guild_ids, logger, startup_tasks)
+        self.reminders.start()
+
+    def cog_unload(self):
+        """
+        Method for cancelling the loop.
+        :return:
+        """
+        self.reminders.cancel()
+
+    @tasks.loop(minutes=1)
+    async def reminders(self):
+        """
+        Loop that triggers reminders
+        :return:
+        """
+
+        now = datetime.datetime.now()
+        for guild in self.bot.guilds:
+
+            open_reminders = self.server_reminders.get_open_reminders(guild.id)
+            for reminder in open_reminders:
+                if now > reminder["timeout"]:
+                    # time to do reminder
+                    channel = await guild.fetch_channel(reminder["channel_id"])
+                    message = await channel.fetch_message(reminder["message_id"])
+
+                    msg = (
+                        f"Hey, <@{reminder['user_id']}>, don't forget about `{reminder['reason']}`!"
+                    )
+
+                    await message.reply(content=msg)
+                    self.server_reminders.close_reminder(reminder["_id"])
+
+    @reminders.before_loop
+    async def before_reminders(self):
+        """
+        Make sure that websocket is open before we start querying via it.
+        :return:
+        """
+        await self.bot.wait_until_ready()
+        while not self._check_start_up_tasks():
+            await asyncio.sleep(5)

c70374b198ab5f6363f2caec5e4f8b0e639098a5
esloman <elliot.sloman@hotmail.com>
2023-04-21 14:05:37 +0100

    #121 - Add impartial button
    Allow users to be 'impartial'. This means users can make a conscious decision to not care! Either not doing anything at all or resetting their existing choice without actively choosing the other faction.

--- discordbot/views/revolution.py
+++ discordbot/views/revolution.py
@@ -50,11 +50,16 @@ class RevolutionView(discord.ui.View):
 
         Defines some args based on the button pressed for use throughout the logic.
 
-        If we're support/overthrow:
+        Non king button:
             - check that we haven't already done the action we're pressing
             - check that we're not locked in to supporting
+
+        If we're support/overthrow:
             - if we previously pressed the other button, reverse those effects
             - apply effects of this button
+        If we're impartial:
+            - remove from revolutionaries/supporters
+            - increase/decrease chance accordingly
         If button is KING button:
             - calculate eddies to remove from King
             - remove those eddies
@@ -95,14 +100,11 @@ class RevolutionView(discord.ui.View):
         user_id = interaction.user.id
         guild_id = interaction.guild.id
 
-        our_user = self.user_points.find_user(
-            user_id,
-            guild_id,
-            {"points": True, "king": True}
-        )
+        guild_db = self.guilds.get_guild(guild_id)
+        king_id = guild_db["king"]
 
         # check that the King isn't using buttons they shouldn't
-        if our_user.get("king", False) and button.label != "Save THYSELF":
+        if (user_id == king_id) and button.label != "Save THYSELF":
             await followup.send(
                 content="You ARE the King - you can't overthrow/support yourself.",
                 ephemeral=True
@@ -112,7 +114,7 @@ class RevolutionView(discord.ui.View):
             return
 
         # check that users aren't using buttons they shouldn't
-        if button.label == "Save THYSELF" and event["king"] != interaction.user.id:
+        if button.label == "Save THYSELF" and (user_id != king_id):
             await followup.send(content="You're not the King - so you can't use this button.", ephemeral=True)
             self.toggle_stuff(False)
             await followup.edit_message(interaction.message.id, view=self)
@@ -135,13 +137,19 @@ class RevolutionView(discord.ui.View):
             case "Save THYSELF":
                 act_type = ActivityTypes.REV_SAVE
                 msg = "Congrats - you've reduced the overthrow chance."
+            case "Impartial":
+                faction_event_key = "neutrals"
+                act_type = ActivityTypes.REV_NEUTRAL
+                msg = "Congrats - you're now impartial."
 
-        if button.label in ["OVERTHROW", "SUPPORT_THE_KING"]:
-            # logic for overthrow/supporting
-            # different to king button logic
+        if button.label != "Save THYSELF":
+            # only do this for the non-KING buttons
+
+            if user_id not in event["users"]:
+                event["users"].append(user_id)
 
             # make sure user isn't doing the same thing twice
-            if user_id in event[faction_event_key]:
+            if user_id in event.get(faction_event_key, []):
                 await followup.send(
                     content="You've already acted on this - you cannot do so again",
                     ephemeral=True
@@ -160,6 +168,10 @@ class RevolutionView(discord.ui.View):
                 await followup.edit_message(interaction.message.id, view=self)
                 return
 
+        if button.label in ["OVERTHROW", "SUPPORT THE KING"]:
+            # logic for overthrow/supporting
+            # different to king button logic
+
             # reverse actions of other faction
             if user_id in event[other_faction_event_key]:
                 event["chance"] -= (faction_chance * -1)
@@ -169,14 +181,32 @@ class RevolutionView(discord.ui.View):
             event[faction_event_key].append(user_id)
             event["chance"] += faction_chance
 
-            if user_id not in event["users"]:
-                event["users"].append(user_id)
+            # remove user from neutrals if they're in it
+            if user_id in event.get("neutrals", []):
+                event["neutrals"].remove(user_id)
+
+        elif button.label == "Impartial":
+            # logic for user pressing the impartial button
+
+            if user_id in event["revolutionaries"]:
+                event["chance"] -= 15
+                event["revolutionaries"].remove(user_id)
+            elif user_id in event["supporters"]:
+                event["chance"] += 15
+                event["supporters"].remove(user_id)
+
+            if user_id not in event["neutrals"]:
+                event["neutrals"].append(user_id)
 
         elif button.label == "Save THYSELF":
             # logic for saving thyself
             # it is different to supporter/revolutionary logic
 
             # work out how many eddies to subtract
+            our_user = self.user_points.find_user(
+                user_id, guild_id,
+                {"points": True, "king": True}
+            )
             eddies = our_user["points"]
             amount_to_subtract = math.floor(eddies * 0.1)
             self.user_points.increment_points(
@@ -201,6 +231,7 @@ class RevolutionView(discord.ui.View):
                 "chance": event["chance"],
                 "supporters": event["supporters"],
                 "revolutionaries": event["revolutionaries"],
+                "neutrals": event["neutrals"],
                 "users": event["users"],
                 "times_saved": event["times_saved"]
             }}
@@ -214,8 +245,6 @@ class RevolutionView(discord.ui.View):
         )
 
         # build new message and update it with updated event
-        guild_db = self.guilds.get_guild(guild_id)
-        king_id = guild_db["king"]
         king_user = await self.client.fetch_user(king_id)  # type: discord.User
         guild = await self.client.fetch_guild(guild_id)
         role = guild.get_role(guild_db["role"])
@@ -250,12 +279,29 @@ class RevolutionView(discord.ui.View):
     )
     async def support_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         """
-        Callback for the SUPPORT button button.
+        Callback for the SUPPORT button.
         Defers to the button logic method for handling the logic.
 
         Args:
             button (discord.ui.Button): the button being pressed
             interaction (discord.Interaction): the interaction context
+        """
+        await self._revolution_button_logic(interaction, button)
+
+    @discord.ui.button(
+        label="Impartial",
+        style=discord.ButtonStyle.blurple,
+        custom_id="impartial_button",
+        emoji="😐"
+    )
+    async def impartial_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+        """
+        Callback for the impartial button.
+        Defers to the button logic method for handling the logic.
+
+        Args:
+            button (discord.ui.Button): _description_
+            interaction (discord.Interaction): _description_
         """
         await self._revolution_button_logic(interaction, button)
 

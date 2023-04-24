b66b3f338a8d0b536769b50cb52330a7ad79349b
esloman <elliot.sloman@hotmail.com>
2023-04-24 17:54:29 +0100

    Bug fixes (#397)
    Fixes #396; duplicated `user_id` entry
    Resolve some errors in the logs with `NotFound` errors with view timeouts

--- discordbot/views/place.py
+++ discordbot/views/place.py
@@ -30,7 +30,13 @@ class PlaceABetView(discord.ui.View):
     async def on_timeout(self):
         for child in self.children:
             child.disabled = True
-        await self.message.edit(content="This `place` command timed out - please _place_ another one", view=None)
+
+        try:
+            await self.message.edit(content="This `place` command timed out - please _place_ another one", view=None)
+        except discord.NotFound:
+            # not found is when the message has already been deleted
+            # don't need to edit in that case
+            pass
 
     @discord.ui.button(label="Submit", style=discord.ButtonStyle.green, row=3, disabled=True, emoji="💰")
     async def submit_button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):

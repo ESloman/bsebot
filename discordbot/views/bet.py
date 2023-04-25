db783bb4e0646245a9099ad39349715b583b8f33
esloman <elliot.sloman@hotmail.com>
2023-04-25 17:52:29 +0100

    #241 - Allow users to change their bets
    This allows users to change their bets if it's been less than ten minutes than when they bet or less than ten minutes since the bet was last updated

--- discordbot/views/bet.py
+++ discordbot/views/bet.py
@@ -1,9 +1,12 @@
 
+import datetime
+
 import discord
 
 from discordbot.modals.addoption import AddBetOption
 from discordbot.slashcommandeventclasses.close import CloseBet
 from discordbot.slashcommandeventclasses.place import PlaceBet
+from discordbot.views.betchange import BetChange
 
 from mongo.bsepoints.bets import UserBets
 from mongo.datatypes import Bet
@@ -47,6 +50,33 @@ class BetView(discord.ui.View):
 
         option_modal = AddBetOption(_bet, self.place, self.close)
         await interaction.response.send_modal(option_modal)
+
+    @discord.ui.button(label="Change your bet", style=discord.ButtonStyle.gray)
+    async def change_callback(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:
+
+        _bet = self.user_bets.get_bet_from_id(self.bet["guild_id"], self.bet["bet_id"])
+
+        if str(interaction.user.id) not in _bet["betters"]:
+            message = "You can't change your bet for a bet you haven't bet on yet."
+            await interaction.response.send_message(content=message, ephemeral=True, delete_after=10)
+            return
+
+        _bet = self.user_bets.get_bet_from_id(self.bet["guild_id"], self.bet["bet_id"])
+
+        if not _bet["active"] or _bet.get("closed", False):
+            message = "Bet is closed - you can't change your bet."
+            await interaction.response.send_message(content=message, ephemeral=True, delete_after=10)
+            return
+
+        first_bet_time = _bet["betters"][str(interaction.user.id)]["first_bet"]
+        now = datetime.datetime.now()
+        if (now - first_bet_time).seconds > 600 and (now - _bet.get("updated", _bet["created"])).seconds > 600:
+            message = "It's been too long since your original bet - you can't change it now."
+            await interaction.response.send_message(content=message, ephemeral=True, delete_after=10)
+            return
+
+        _view = BetChange(_bet, self.place, self.close)
+        await interaction.response.send_message(content="Select another bet option.", view=_view, ephemeral=True)
 
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red, emoji="✖️")
     async def cancel_callback(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:

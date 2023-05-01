613debb39d300649a67ae2c1e9b93a0cc4ce4c9e
esloman <elliot.sloman@hotmail.com>
2023-05-01 13:24:08 +0100

    Some bug fixes (#413)
    - Fix error with some slash commands not working
    - Remove UTC stuff from BSEddies

--- discordbot/slashcommandeventclasses/pledge.py
+++ discordbot/slashcommandeventclasses/pledge.py
@@ -16,7 +16,7 @@ class Pledge(BSEddies):
             return
 
         self._add_event_type_to_activity_history(
-            ctx.user, ctx.guild_id, ActivityTypes.BSEDDIES_PLEDGE, user_id=ctx.user.id
+            ctx.user, ctx.guild_id, ActivityTypes.BSEDDIES_PLEDGE
         )
 
         guild_id = ctx.guild.id

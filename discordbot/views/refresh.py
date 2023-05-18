7c6bd8b3d38b8e722cdd0dc6b84cbe84500002d5
esloman <elliot.sloman@hotmail.com>
2023-05-18 08:08:41 +0100

    #382 - Markdown changes (#387)
    * #382 - Update markdown stuff
    
    Beginnings of updating bot messages to better leverage the new markdown capabilities
    
    * #382 - Markdown stuff
    
    Make one of the vally messages super large
    Add some more wordle reminder messages
    
    * #382 - Markdown stuff
    
    Update to revert back to embed and with formatted title
    
    * #382  - Markdown stuff
    
    Add a random embed colour
    
    * Fix typo
    
    * Update help.py
    
    Add remind me help options
    Wanted to have this formatted with markdown without merge errors so added it here. Remind me functionality added as part of #293.
    
    * Make sure new suggest modal has markdown changes
    
    * #387 - Markdown changes
    
    Make more changes to markdown stuff with the new things we've added recently

--- discordbot/views/refresh.py
+++ discordbot/views/refresh.py
@@ -42,8 +42,12 @@ class RefreshBetView(discord.ui.View):
         channel = await interaction.guild.fetch_channel(bet["channel_id"])
         message = await channel.fetch_message(bet["message_id"])
         embed = self.embed_manager.get_bet_embed(interaction.guild, bet_id, bet)
+        content = (
+            f"# {bet['title']}\n"
+            f"_Created by <@{bet['user']}>_"
+        )
         view = BetView(bet, self.bseddies_place, self.bseddies_close)
-        await message.edit(embed=embed, view=view)
+        await message.edit(content=content, view=view, embed=embed)
         await interaction.response.edit_message(content="Refreshed the bet for you.", view=None)
 
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red, row=3, disabled=False, emoji="✖️")

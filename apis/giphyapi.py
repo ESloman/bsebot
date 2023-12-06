c37d6f691aae23ec7d73534a7b4458390c2a7a45
esloman <elliot.sloman@hotmail.com>
2023-12-06 11:33:56 +0000

    Linting API classes

--- apis/giphyapi.py
+++ apis/giphyapi.py
@@ -1,19 +1,31 @@
+"""Guphy API class."""
+
 import aiohttp
 
 
-class GiphyAPI(object):
-    """
-    API Class for interacting with Giphy API
-    """
-    def __init__(self, token):
-        """
-        Constructor this class. Needs a GIPHY API Token
-        :param token:
+class GiphyAPI:
+    """API Class for interacting with Giphy API."""
+
+    def __init__(self: "GiphyAPI", token: str) -> None:
+        """Initialise this class.
+
+        Needs a GIPHY API Token/
+
+        Args:
+            token (str): the token
         """
         self.token = token
         self.api_path = "https://api.giphy.com/v1/"
 
-    async def random_gif(self, tag: str = None) -> str:
+    async def random_gif(self, tag: str | None = None) -> str:
+        """Generates a random gif with the given tag.
+
+        Args:
+            tag (str, optional): the tag to search for. Defaults to None.
+
+        Returns:
+            str: the link to the gif
+        """
         url = self.api_path + "gifs/random"
 
         params = {"api_key": self.token}
@@ -21,7 +33,6 @@ class GiphyAPI(object):
         if tag:
             params["tag"] = tag
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params) as response:
-                ret = await response.json()
+        async with aiohttp.ClientSession() as session, session.get(url, params=params) as response:
+            ret = await response.json()
         return ret["data"]["images"]["original_still"]["url"]

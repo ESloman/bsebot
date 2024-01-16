209f88a78031b339b105206ac7b817919d07ef55
ESloman <elliot.sloman@hotmail.com>
2024-01-16 05:26:31 +0000

    Add some tests for interface

--- tests/mocks/mongo_mocks.py
+++ tests/mocks/mongo_mocks.py
@@ -8,7 +8,93 @@ from mongo.datatypes.guild import GuildDB
 from mongo.datatypes.user import UserDB
 
 
+class MockClient:
+    """Mock client class."""
+
+    def __init__(self, *_args, **_kwargs) -> None:
+        """Initialisation method."""
+
+    def list_database_names(self) -> list[str]:
+        """Mock list_database_names method."""
+        return [
+            "database_name",
+            "another_database_name",
+        ]
+
+
+class MockDatabase:
+    """Mock database class."""
+
+    def __init__(self, *_args, **_kwargs) -> None:
+        """Initialisation method."""
+
+    def list_collection_names(self, include_system_collections: bool = False) -> list[str]:
+        """Mock list_collection_names method."""
+        return [
+            "collection_name",
+            "another_collecton_name",
+        ]
+
+
+class MockResult:
+    """Mock result class."""
+
+    def __init__(self, num: int) -> None:
+        """Initialisation method."""
+        self._ids = [ObjectId() for _ in range(num)]
+
+    @property
+    def inserted_ids(self) -> list[ObjectId]:
+        """Mock inserted_ids property."""
+        return self._ids
+
+    @property
+    def deleted_count(self) -> int:
+        """Mock deleted_count property."""
+        return len(self._ids)
+
+
+class MockCollection:
+    """Mock collection class."""
+
+    def __init__(self, *_args, **_kwargs) -> None:
+        """Initialisation method."""
+
+    def insert_many(self, documents: list[dict], ordered: bool = False):
+        """Mock insert_many method."""
+        return MockResult(len(documents))
+
+    def delete_many(self, parameters: dict[str, any]):
+        """Mock delete_many method."""
+        return MockResult(5)
+
+    def delete_one(self, parameters: dict[str, any]):
+        """Mock delete_one method."""
+        return MockResult(1)
+
+    def update_many(self, filter: dict[str, any], update: dict[str, any], upsert: bool = False):  # noqa: A002
+        """Mock update_many method."""
+        return MockResult(5)
+
+    def update_one(self, filter: dict[str, any], update: dict[str, any], upsert: bool = False):  # noqa: A002
+        """Mock update_one method."""
+        return MockResult(1)
+
+    def find(
+        self,
+        params: dict[str, any],
+        limit: int = 1000,
+        projection: dict[str, any] | None = None,
+        skip: int = 0,
+        sort: list[tuple] | None = None,
+    ):
+        """Mock find method."""
+        return ({"key": f"value{x}"} for x in range(5))
+
+
 class UserPointsMock:
+    """Mock UserPoints class."""
+
     def get_all_users_for_guild(self, guild_id: int) -> list[UserDB]:
         """Mocks getting all users."""
         return [
@@ -39,6 +125,8 @@ class UserPointsMock:
 
 
 class GuildsMock:
+    """Mock Guilds class."""
+
     def get_guild(self, guild_id: int) -> GuildDB:
         """Mock for get_guild."""
         return GuildDB(_id=ObjectId(), guild_id=guild_id, king=3, admins=[], owner_id=guild_id, name="somename")
--- tests/test_mongo/test_interface.py
+++ tests/test_mongo/test_interface.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 
 from mongo import interface
 from mongo.interface import CachedMongoClient
+from tests.mocks.mongo_mocks import MockClient, MockCollection, MockDatabase
 
 
 class TestCachedMongoClient:
@@ -35,3 +36,75 @@ class TestGetClient:
         client = interface.get_client(user_name="user", password="password")
         assert isinstance(client, MongoClient)
         assert interface.get_client(user_name="user", password="password") is client
+
+
+class TestClientMethods:
+    """Tests our client methods."""
+
+    def test_list_database_names(self) -> None:
+        """Tests interface list_database_names method."""
+        client = MockClient()
+        database_names = interface.get_database_names(client)
+        assert isinstance(database_names, list)
+
+    def test_list_collection_names(self) -> None:
+        """Tests interface list_collection_names method."""
+        database = MockDatabase()
+        collection_names = interface.get_collection_names(database)
+        assert isinstance(collection_names, list)
+
+    def test_insert_single(self) -> None:
+        """Tests interface insert with a single document."""
+        collection = MockCollection()
+        doc = {"key": "value"}
+        result = interface.insert(collection, doc)
+        assert isinstance(result, list)
+
+    def test_insert_multiple(self) -> None:
+        """Tests interface insert with multiple documents."""
+        collection = MockCollection()
+        docs = [{"key": f"value{x}"} for x in range(10)]
+        result = interface.insert(collection, docs)
+        assert isinstance(result, list)
+
+    def test_delete_single(self) -> None:
+        """Tests interface delete with not many."""
+        collection = MockCollection()
+        params = {"key": "value"}
+        result = interface.delete(collection, params, many=False)
+        assert result == 1
+
+    def test_delete_multiple(self) -> None:
+        """Tests interface delete with many."""
+        collection = MockCollection()
+        params = {"key": "value"}
+        result = interface.delete(collection, params, many=True)
+        assert result >= 1
+
+    def test_update_single(self) -> None:
+        """Tests interface update with not many."""
+        collection = MockCollection()
+        params = {"key": "value"}
+        update = {"$set": {"key": 123}}
+        interface.update(collection, params, update, many=False)
+
+    def test_update_multiple(self) -> None:
+        """Tests interface update with many."""
+        collection = MockCollection()
+        params = {"key": "value"}
+        update = {"$set": {"key": 123}}
+        interface.update(collection, params, update, many=True)
+
+    def test_query_not_list(self) -> None:
+        """Tests interface query as not list."""
+        collection = MockCollection()
+        params = {"key": "value"}
+        results = interface.query(collection, params, as_gen=True)
+        assert not isinstance(results, list)
+
+    def test_query_list(self) -> None:
+        """Tests interface query as list."""
+        collection = MockCollection()
+        params = {"key": "value"}
+        results = interface.query(collection, params, skip=0, as_gen=False)
+        assert isinstance(results, list)

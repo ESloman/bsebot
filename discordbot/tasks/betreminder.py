bb4a3509a077846369f64b1b3a8f4fd8781fe887
esloman <elliot.sloman@hotmail.com>
2024-01-29 13:37:12 +0000

    Refactor this class to reduce complexity

--- discordbot/tasks/betreminder.py
+++ discordbot/tasks/betreminder.py
@@ -9,6 +9,7 @@ from discord.ext import tasks
 
 from discordbot.bsebot import BSEBot
 from discordbot.tasks.basetask import BaseTask
+from mongo.datatypes.bet import BetDB
 
 
 class BetReminder(BaseTask):
@@ -28,76 +29,102 @@ class BetReminder(BaseTask):
         self.task = self.bet_reminder
         self.task.start()
 
+    async def _check_bet_for_halfway_reminder(self, bet: BetDB, now: datetime.datetime) -> bool:
+        """Checks to see if a bet needs a 'halfway' to go reminder.
+
+        Args:
+            bet (BetDB): the bet to check
+            now (datetime.datetime): the datetime
+
+        Returns:
+            bool: whether we sent a message or not
+        """
+        total_time = (bet.timeout - bet.created).total_seconds()
+        half_time = total_time / 2
+        half_date = bet.created + datetime.timedelta(seconds=half_time)
+
+        if now > half_date:
+            return False
+
+        half_diff = half_date - now
+
+        if half_diff.total_seconds() < 3600:  # noqa: PLR2004
+            # within the hour threshold for half way
+            channel = await self.bot.fetch_channel(bet.channel_id)
+            await channel.trigger_typing()
+            message = await channel.fetch_message(bet.message_id)
+
+            num_betters = len(bet.betters.keys())
+            eddies_bet = self.user_bets.count_eddies_for_bet(bet)
+
+            msg = (
+                "About halfway to go on this bet - don't forget to place some eddies!"
+                f"Current there's `{eddies_bet}` eddies on the line from **{num_betters}** betters."
+            )
+
+            try:
+                _place_command = next(a for a in self.bot.application_commands if a.name == "place")
+                msg += f"\n\nUse {_place_command.mention} to place some eddies."
+            except (IndexError, AttributeError, TypeError):
+                pass
+
+            await message.reply(content=msg, silent=True)
+            return True
+        return False
+
+    async def _check_bet_for_day_reminder(self, bet: BetDB, now: datetime.datetime) -> bool:
+        """Checks to see if a bet needs a day before reminder.
+
+        Args:
+            bet (BetDB): the bet to check
+            now (datetime.datetime): the current datetime
+
+        Returns:
+            bool: whether we sent a message or not
+        """
+        total_time = (bet.timeout - bet.created).total_seconds()
+
+        if total_time <= 604800 or bet.timeout < now:  # noqa: PLR2004
+            # if bet timeout is less than a week - don't bother with halfway reminders
+            return False
+
+        diff = bet.timeout - now
+
+        if 82800 <= diff.total_seconds() <= 86400:  # noqa: PLR2004
+            # ~ 24 hours to go!
+            # send reminder here
+            channel = await self.bot.fetch_channel(bet.channel_id)
+            await channel.trigger_typing()
+            message = await channel.fetch_message(bet.message_id)
+
+            num_betters = len(bet.betters.keys())
+            eddies_bet = self.user_bets.count_eddies_for_bet(bet)
+
+            msg = (
+                "Only roughly twenty four hours to get in on this bet!\n"
+                f"Current there's `{eddies_bet}` eddies on the line from **{num_betters}** betters."
+            )
+
+            try:
+                _place_command = next(a for a in self.bot.application_commands if a.name == "place")
+                msg += f"\n\nUse {_place_command.mention} to place some eddies."
+            except (IndexError, AttributeError, TypeError):
+                pass
+
+            await message.reply(content=msg)
+            return True
+        return False
+
     @tasks.loop(minutes=60)
     async def bet_reminder(self) -> None:
         """Loop that takes all our active bets and sends a reminder message."""
         now = datetime.datetime.now(tz=pytz.utc)
         for guild in self.bot.guilds:
-            await self.bot.fetch_guild(guild.id)  # type: discord.Guild
             active = self.user_bets.get_all_active_bets(guild.id)
             for bet in active:
-                timeout = bet.timeout
-                created = bet.created
-                total_time = (timeout - created).total_seconds()
-
-                if total_time <= 604800:  # noqa: PLR2004
-                    # if bet timeout is less than a week - don't bother with halfway reminders
-                    continue
-
-                if now > timeout:
-                    continue
-
-                diff = timeout - now
-
-                if 82800 <= diff.total_seconds() <= 86400:  # noqa: PLR2004
-                    # ~ 24 hours to go!
-                    # send reminder here
-                    channel = await self.bot.fetch_channel(bet.channel_id)
-                    await channel.trigger_typing()
-                    message = await channel.fetch_message(bet.message_id)
-
-                    num_betters = len(bet.betters.keys())
-                    eddies_bet = self.user_bets.count_eddies_for_bet(bet)
-
-                    msg = (
-                        "Only roughly twenty four hours to get in on this bet!\n"
-                        f"Current there's `{eddies_bet}` eddies on the line from **{num_betters}** betters."
-                    )
-
-                    try:
-                        _place_command = next(a for a in self.bot.application_commands if a.name == "place")
-                        msg += f"\n\nUse {_place_command.mention} to place some eddies."
-                    except (IndexError, AttributeError, TypeError):
-                        pass
-
-                    await message.reply(content=msg)
-                    continue
-
-                half_time = total_time / 2
-                half_date = created + datetime.timedelta(seconds=half_time)
-
-                if now > half_date:
-                    continue
-
-                half_diff = half_date - now
-
-                if half_diff.total_seconds() < 3600:  # noqa: PLR2004
-                    # within the hour threshold for half way
-                    channel = await self.bot.fetch_channel(bet.channel_id)
-                    await channel.trigger_typing()
-                    message = await channel.fetch_message(bet.message_id)
-
-                    eddies_bet = self.user_bets.count_eddies_for_bet(bet)
-
-                    msg = "About halfway to go on this bet - don't forget to place some eddies!"
-
-                    try:
-                        _place_command = next(a for a in self.bot.application_commands if a.name == "place")
-                        msg += f"\n\nUse {_place_command.mention} to place some eddies."
-                    except (IndexError, AttributeError, TypeError):
-                        pass
-
-                    await message.reply(content=msg, silent=True)
+                if not await self._check_bet_for_day_reminder(bet, now):
+                    # only want to send the "halfway" reminder if didn't send the 24 hours to go reminder
+                    await self._check_bet_for_halfway_reminder(bet, now)
 
     @bet_reminder.before_loop
     async def before_bet_reminder(self) -> None:

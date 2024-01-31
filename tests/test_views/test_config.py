184333dc6c6c93dbaa968f6def072895266aba8a
ESloman <elliot.sloman@hotmail.com>
2024-01-31 10:22:15 +0000

    Further tests for config

--- tests/test_views/test_config.py
+++ tests/test_views/test_config.py
@@ -8,7 +8,7 @@ from discordbot.constants import CREATOR
 from discordbot.utilities import PlaceHolderLogger
 from discordbot.views.config import ConfigView
 from mongo import interface
-from tests.mocks import bsebot_mocks, interface_mocks
+from tests.mocks import bsebot_mocks, discord_mocks, interface_mocks
 
 
 class TestConfigView:
@@ -55,6 +55,15 @@ class TestConfigView:
         Needs to run with async as the parent class tries to get the running event loop.
         """
         _ = ConfigView(self.bsebot, user_data["uid"], guild_data["guild_id"])
+
+    async def test_send_no_perms_message(self) -> None:
+        """Tests send no perms message.
+
+        Needs to run with async as the parent class tries to get the running event loop.
+        """
+        config = ConfigView(self.bsebot)
+        interaction = discord_mocks.InteractionMock(123456)
+        await config._send_no_perms_message(interaction)
 
     @pytest.mark.parametrize("value", ["daily_salary", "activities", "wordle_starting_words", "something"])
     @pytest.mark.parametrize("user_data", [*interface_mocks.query_mock("userpoints", {})[-5:], {"uid": CREATOR}])

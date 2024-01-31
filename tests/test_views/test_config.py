67e47501a188ec75bd5620095694468d6c5f1c4b
ESloman <elliot.sloman@hotmail.com>
2024-01-31 10:15:28 +0000

    Starting adding tests for config view

---
+++ tests/test_views/test_config.py
@@ -0,0 +1,70 @@
+"""Tests our revolution views."""
+
+from unittest import mock
+
+import pytest
+
+from discordbot.constants import CREATOR
+from discordbot.utilities import PlaceHolderLogger
+from discordbot.views.config import ConfigView
+from mongo import interface
+from tests.mocks import bsebot_mocks, interface_mocks
+
+
+class TestConfigView:
+    """Tests our RevolutionView view."""
+
+    @pytest.fixture(autouse=True)
+    def _test_data(self) -> None:
+        """Fixture to get test data.
+
+        Automatically called before each test.
+        """
+        self.bsebot = bsebot_mocks.BSEBotMock()
+        self.logger = PlaceHolderLogger
+
+    @mock.patch.object(interface, "get_collection", new=interface_mocks.get_collection_mock)
+    @mock.patch.object(interface, "get_database", new=interface_mocks.get_database_mock)
+    @mock.patch.object(interface, "query", new=interface_mocks.query_mock)
+    async def test_init(self) -> None:
+        """Tests basic init.
+
+        Needs to run with async as the parent class tries to get the running event loop.
+        """
+        _ = ConfigView(self.bsebot)
+
+    @pytest.mark.parametrize("user_data", interface_mocks.query_mock("userpoints", {})[-5:])
+    @mock.patch.object(interface, "get_collection", new=interface_mocks.get_collection_mock)
+    @mock.patch.object(interface, "get_database", new=interface_mocks.get_database_mock)
+    @mock.patch.object(interface, "query", new=interface_mocks.query_mock)
+    async def test_init_with_user_id_not_guild_id(self, user_data: dict) -> None:
+        """Tests basic init with user_id and not guild id.
+
+        Needs to run with async as the parent class tries to get the running event loop.
+        """
+        _ = ConfigView(self.bsebot, user_data["uid"])
+
+    @pytest.mark.parametrize("guild_data", interface_mocks.query_mock("guilds", {})[-5:])
+    @pytest.mark.parametrize("user_data", interface_mocks.query_mock("userpoints", {})[-5:])
+    @mock.patch.object(interface, "get_collection", new=interface_mocks.get_collection_mock)
+    @mock.patch.object(interface, "get_database", new=interface_mocks.get_database_mock)
+    @mock.patch.object(interface, "query", new=interface_mocks.query_mock)
+    async def test_init_with_values(self, guild_data: dict, user_data: dict) -> None:
+        """Tests basic init with guild and user data.
+
+        Needs to run with async as the parent class tries to get the running event loop.
+        """
+        _ = ConfigView(self.bsebot, user_data["uid"], guild_data["guild_id"])
+
+    @pytest.mark.parametrize("value", ["daily_salary", "activities", "wordle_starting_words", "something"])
+    @pytest.mark.parametrize("user_data", [*interface_mocks.query_mock("userpoints", {})[-5:], {"uid": CREATOR}])
+    @mock.patch.object(interface, "get_collection", new=interface_mocks.get_collection_mock)
+    @mock.patch.object(interface, "get_database", new=interface_mocks.get_database_mock)
+    @mock.patch.object(interface, "query", new=interface_mocks.query_mock)
+    async def test_check_perms(self, value: str, user_data: dict) -> None:
+        """Tests check perms with basic values and some users.
+
+        Needs to run with async as the parent class tries to get the running event loop.
+        """
+        config = ConfigView(self.bsebot, user_data["uid"])
+        config._check_perms(value, user_data["uid"])

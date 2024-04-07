ebf2f738bf4d8873a450fb05e8943a7978fbe0d7
ESloman <elliot.sloman@hotmail.com>
2024-04-07 16:45:03 +0100

    test: add more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -30,3 +30,36 @@ class TestMain:
         main.TOKEN = "token"
         with mock.patch.object(main.BSEBot, "run"):
             main._main()
+
+    @mock.patch.object(interface, "get_collection", new=interface_mocks.get_collection_mock)
+    @mock.patch.object(interface, "get_database", new=interface_mocks.get_database_mock)
+    @mock.patch.object(interface, "query", new=interface_mocks.query_mock)
+    @mock.patch.dict(os.environ, {"MONGODB_IP": "localhost"})
+    def test_main_with_mongo_ip(self) -> None:
+        """Tests main function with mongo ip set through an environment variable."""
+        main.DEBUG_MODE = True
+        main.TOKEN = "token"
+        with mock.patch.object(main.BSEBot, "run"):
+            main._main()
+
+    @pytest.mark.parametrize("get_key", [None, "some_val"])
+    @mock.patch.object(interface, "get_collection", new=interface_mocks.get_collection_mock)
+    @mock.patch.object(interface, "get_database", new=interface_mocks.get_database_mock)
+    @mock.patch.object(interface, "query", new=interface_mocks.query_mock)
+    def test_main_giphy_token(self, get_key: str | None) -> None:
+        """Tests main function with various giphy token .env values."""
+        main.DEBUG_MODE = True
+        main.TOKEN = "token"
+        with mock.patch.object(main.BSEBot, "run"), mock.patch.object(main.dotenv, "get_key", new=lambda *_: get_key):  # noqa: PT008
+            main._main()
+
+    @mock.patch.object(interface, "get_collection", new=interface_mocks.get_collection_mock)
+    @mock.patch.object(interface, "get_database", new=interface_mocks.get_database_mock)
+    @mock.patch.object(interface, "query", new=interface_mocks.query_mock)
+    @mock.patch.dict(os.environ, {"GIPHY_TOKEN": "token"})
+    def test_main_giphy_env(self) -> None:
+        """Tests main function with giphy token set via environment variable."""
+        main.DEBUG_MODE = True
+        main.TOKEN = "token"
+        with mock.patch.object(main.BSEBot, "run"):
+            main._main()

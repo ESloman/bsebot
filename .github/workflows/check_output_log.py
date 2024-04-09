9aabe0aa65740723ecbf304c10070854de767fc7
ESloman <elliot.sloman@hotmail.com>
2024-04-09 08:18:19 +0100

    ci: tweaks again

--- .github/workflows/check_output_log.py
+++ .github/workflows/check_output_log.py
@@ -1,7 +1,7 @@
-"""Checks the docker logs output of bsebot to check for errors."""
+"""Checks the output in output.log for any errors."""
 
-import subprocess  # noqa: S404
 import sys
+from pathlib import Path
 
 DEBUG: bool = False
 
@@ -10,19 +10,15 @@ if __name__ == "__main__":
         print("Debug mode enabled.")
         DEBUG = True
 
-    bytes_output: bytes = subprocess.check_output(["docker", "logs", "bsebot"])  # noqa: S603, S607
-    output: list[str] = str(bytes_output).split("\n")
-
-    if DEBUG:
-        print(f"Length of bytes output is: {len(bytes_output)}")
-        print(f"Length of output is: {len(output)}")
-
+    path: Path = Path(".", "bsebotlogs", "bsebot.log")
     errors: int = 0
-    for line in output:
-        if DEBUG:
-            print(line)
-        if any(x in line.lower() for x in ["error", "traceback", "exception"]):
-            errors += 1
+    with open(path, encoding="utf-8") as output:
+        for line in output:
             if DEBUG:
-                print(f"Line '{line.strip()}' was NOT clean - increased error count to {errors=}.")
+                print(line)
+
+            if any(x in line.lower() for x in ["error", "traceback", "exception"]):
+                errors += 1
+                if DEBUG:
+                    print(f"Line '{line.strip()}' was NOT clean - increased error count to {errors=}.")
     print(errors)

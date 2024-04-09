c64c7fdec929dc44afc2f9bf644008576fef4447
esloman <elliot.sloman@hotmail.com>
2024-04-09 09:57:56 +0200

    ci: fix CI issues (#623)
    Ensure that we are actually checking for errors after we run the BSEBot container for a couple of minutes.

---
+++ .github/workflows/check_output_log.py
@@ -0,0 +1,24 @@
+"""Checks the output in output.log for any errors."""
+
+import sys
+from pathlib import Path
+
+DEBUG: bool = False
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "-d":
+        print("Debug mode enabled.")
+        DEBUG = True
+
+    path: Path = Path(".", "bsebotlogs", "bsebot.log")
+    errors: int = 0
+    with open(path, encoding="utf-8") as output:
+        for line in output:
+            if DEBUG:
+                print(line)
+
+            if any(x in line.lower() for x in ["error", "traceback", "exception"]):
+                errors += 1
+                if DEBUG:
+                    print(f"Line '{line.strip()}' was NOT clean - increased error count to {errors=}.")
+    print(errors)

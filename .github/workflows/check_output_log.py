56183517855dea92fa366ab217764e85984d1a4c
ESloman <elliot.sloman@hotmail.com>
2024-04-09 08:07:39 +0100

    ci: tweak script

--- .github/workflows/check_output_log.py
+++ .github/workflows/check_output_log.py
@@ -12,12 +12,17 @@ if __name__ == "__main__":
 
     bytes_output: bytes = subprocess.check_output(["docker", "logs", "bsebot"])  # noqa: S603, S607
     output: list[str] = str(bytes_output).split("\n")
+
+    if DEBUG:
+        print(f"Length of bytes output is: {len(bytes_output)}")
+        print(f"Length of output is: {len(output)}")
+
     errors: int = 0
     for line in output:
+        if DEBUG:
+            print(line)
         if any(x in line.lower() for x in ["error", "traceback", "exception"]):
             errors += 1
             if DEBUG:
                 print(f"Line '{line.strip()}' was NOT clean - increased error count to {errors=}.")
-        elif DEBUG:
-            print(f"Line '{line.lower()}' was clean.")
     print(errors)
